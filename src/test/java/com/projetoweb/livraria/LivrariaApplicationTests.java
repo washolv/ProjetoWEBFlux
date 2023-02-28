2833fea626370c63a8deb80a7566fbe1b29b0483
washolv <w_olv@hotmail.com>
2023-02-27 21:37:08 -0300

    first commit

---
+++ src/test/java/com/projetoweb/livraria/LivrariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoweb.livraria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
